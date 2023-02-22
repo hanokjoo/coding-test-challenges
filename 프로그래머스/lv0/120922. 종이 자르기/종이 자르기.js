ec7a52361646f085379fc3e2af55599e31215da3
Okjoo Han <okjoo.han88@gmail.com>
2023-02-22 16:00:50 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
@@ -1,5 +1,8 @@
 function solution(M, N) {
     // 가로 가위질 m - 1번
     // 세로 가위질 (n - 1) * m번
-    return (M - 1) + ((N - 1) * M);
+    // = M - 1 + (N * M) - M
+    // = M * N - 1
+    // retrn (M - 1) + ((N - 1) * M);
+    return M * N -1;
 }
