880ef35f0acb3d259efdafb150ef668bd51f2d2c
Okjoo Han <okjoo.han88@gmail.com>
2023-03-02 23:34:52 +0900

    [level 0] Title: k의 개수, Time: 9.14 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -6,4 +6,8 @@ function solution(i, j, k) {
     }
     
     return array.join("").replace(new RegExp(`[^${k}]`, "g"), "").length;
+    
+    /* 참고할 만한 코드
+    return Array(j-i+1).fill(i).map((v,i)=>v+i).join('').split(k).length-1;
+    */
 }
