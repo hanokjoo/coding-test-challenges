be5ef8a5463fab1067975560e3ca629ba1e04313
Okjoo Han <okjoo.han88@gmail.com>
2023-03-02 23:42:58 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,4 +1,9 @@
 function solution(before, after) {
     return (JSON.stringify([...before].sort()) 
             === JSON.stringify([...after].sort())) ? 1 : 0;
+    
+    /* 참고할 만한 코드 - join()으로 문자열 만들어서 비교하면 됨
+    return before.split('').sort().join('') 
+            === after.split('').sort().join('') ? 1 : 0;
+    */
 }
