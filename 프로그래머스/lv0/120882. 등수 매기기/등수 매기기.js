4687fa3ea031e2ef002a45f697430ad42d11ee3c
Okjoo Han <okjoo.han88@gmail.com>
2023-03-04 01:07:41 +0900

    [level 0] Title: 등수 매기기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -17,4 +17,26 @@ function solution(score) {
     }
     
     return answer;
+    
+    /* 참고할 만한 코드
+    1)
+    return score.map((el) => {
+        return (
+            score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+        );
+    });
+
+    2)
+    let avg = score.map(v=>(v[0]+v[1])/2);
+    let sorted = avg.slice().sort((a,b)=>b-a);
+    return avg.map(v=>sorted.indexOf(v)+1);
+    
+    3)
+    const dict = {};
+    const rank = score
+        .map(([a,b]) => (a+b)/2)
+        .sort((a,b) => b-a)
+        .forEach((a,i) => dict[a] === undefined ? dict[a] = i+1 : null);
+    return score.map(([a,b]) => dict[(a+b)/2]);
+    */
 }
