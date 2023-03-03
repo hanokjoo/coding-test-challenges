db1eeee09d471e664e824e6a2e9b6813dfcc6917
Okjoo Han <okjoo.han88@gmail.com>
2023-03-03 14:45:54 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,4 +1,12 @@
 function solution(chicken) {
     if (chicken < 10) return 0;
     return parseInt(chicken / 10) + solution(parseInt(chicken / 10) + parseInt(chicken % 10));
+    
+    /* 참고할 만한 코드
+    var answer = parseInt((chicken-1) / 9);
+    return answer;
+    
+    return ~~(chicken*0.111111) 
+    -> (1000 * 0.1) + (100 * 0.1) + (10 * 0.1) -> (1000 * 0.1) + (1000 * 0.01) + (1000 * 0.001)
+    */
 }
