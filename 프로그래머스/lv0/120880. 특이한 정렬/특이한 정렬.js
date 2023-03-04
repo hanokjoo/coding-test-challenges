edc855cdc58929194e4a25770a46cf86797d171c
Okjoo Han <okjoo.han88@gmail.com>
2023-03-04 21:32:38 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -2,4 +2,8 @@ function solution(numlist, n) {
     return numlist.map((num) => [Math.abs(num - n), num])
     .sort((a, b) => (a[0] === b[0]) ? b[1] - a[1] : a[0] - b[0])
     .map((obj) => obj[1]);
+    
+    /* 참고할 만한 코드
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
+    */
 }
