7e2df6fefbeb60231a271120d540926c99740e3c
Okjoo Han <okjoo.han88@gmail.com>
2023-03-10 21:04:01 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -2,4 +2,8 @@ function solution(my_string) {
     let numbers = my_string.match(/\d+/g);
     if (numbers !== null) return numbers.reduce((acc, cur) => parseInt(acc) + parseInt(cur), 0);
     else return 0;
+    
+    /* 참고할 만한 코드 - 내 코드의 간략버전
+    return s.match(/(\d+)/g)?.reduce((t,m)=>t+Number(m),0) || 0;
+    */
 }
