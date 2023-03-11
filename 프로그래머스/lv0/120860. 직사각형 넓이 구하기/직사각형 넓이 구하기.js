229d8870d410f23b552b9d80f63342e937f2145f
Okjoo Han <okjoo.han88@gmail.com>
2023-03-11 22:50:26 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -5,4 +5,15 @@ function solution(dots) {
     });
     
     return (dots[2][0] - dots[0][0]) * (dots[1][1] - dots[0][1]);
+    
+    /* 참고할 만한 코드
+     let x = [], y = [];
+
+    for (let pos of dots) {
+        x.push(pos[0]);
+        y.push(pos[1]);
+    }
+
+    return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
+    */
 }
