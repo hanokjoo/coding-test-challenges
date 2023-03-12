1787da42eeb03191081e950a714e372a31bd18e9
Okjoo Han <okjoo.han88@gmail.com>
2023-03-12 21:34:20 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -3,4 +3,8 @@ function solution(my_string) {
         .split("")
         .map((e) => parseInt(e))
         .sort((a, b) => a - b);
+    
+    /* 참고할 만한 코드 - 정규식으로 숫자만 가져와서 정렬하기
+    return my_string.match(/\d/g).sort((a, b) => a - b).map(n => Number(n));
+    */
 }
