f0e50b57f5a4a20b409c0820e965f0cf945a9f8a
Okjoo Han <okjoo.han88@gmail.com>
2023-03-13 21:17:24 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -5,4 +5,9 @@ function solution(numbers, direction) {
         numbers.unshift(numbers.pop());
     }
     return numbers;
+    
+    /* 참고할 만한 코드 - 내 코드의 간략버전
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
+    return numbers;
+    */
 }
