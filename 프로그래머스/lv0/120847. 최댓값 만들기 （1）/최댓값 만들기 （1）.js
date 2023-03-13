75b19cffd738badc0dbed10934565da6b576bbe8
Okjoo Han <okjoo.han88@gmail.com>
2023-03-13 21:01:13 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,3 +1,13 @@
 function solution(numbers) {
     return numbers.sort((a, b) => a - b).slice(-2).reduce((acc, cur) => acc * cur, 1);
+    
+    /* 참고할 만한 코드
+    - 오름차순으로 정리
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
+    
+    - 필요한 두 값만 가져오기
+    let [a, b] = numbers.sort((a,b) => b - a);
+    return a * b;
+    */
 }
