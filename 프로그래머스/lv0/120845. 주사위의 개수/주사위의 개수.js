26132d8e924acca52f5ad4a9b2e45466db569431
Okjoo Han <okjoo.han88@gmail.com>
2023-03-13 21:13:08 +0900

    [level 0] Title: 주사위의 개수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,12 @@
 function solution(box, n) {
     return box.map((e) => parseInt(e / n)).reduce((acc, cur) => acc * cur, 1);
+    
+    /* 참고할 만한 코드
+    - 배열에 변수를 넣어서 선언할 수 있다.
+    let [width, length, height] = box;
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+    
+    - 내 코드의 간략버전
+    return box.reduce((acc,v) => acc * Math.floor(v / n), 1);
+    */
 }
