656f96fb51716104d954ef5f92bfad69f3c61107
Okjoo Han <okjoo.han88@gmail.com>
2023-03-13 22:07:05 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -4,4 +4,13 @@ function solution(num_list, n) {
         answer.push(num_list.slice(i * n, (i + 1) * n));
     }
     return answer;
+    
+    /* 참고할 만한 코드 - splice 활용
+    var answer = [];
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
+    }
+    return answer;
+    */
 }
