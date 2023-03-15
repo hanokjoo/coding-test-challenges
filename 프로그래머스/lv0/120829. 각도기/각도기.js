d9799288906ec98f2b8f6a8d2005974af0d4683d
Okjoo Han <okjoo.han88@gmail.com>
2023-03-15 22:50:30 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -3,4 +3,12 @@ function solution(angle) {
     else if (angle === 90) return 2;
     else if (90 < angle && angle < 180) return 3;
     else if (angle === 180) return 4;
+    
+    /* 참고할 만한 코드
+    - filter 사용
+    return [0, 90, 91, 180].filter(x => angle>=x).length;
+    
+    - 삼항연산자 사용
+    return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
+    */
 }
