79fb06aee063059c0d771cffa7c84e2756520298
Okjoo Han <okjoo.han88@gmail.com>
2023-03-15 22:47:08 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
@@ -1,3 +1,6 @@
 function solution(age) {
-    return new Date().getFullYear() - age;
+    return 2022 - age + 1; 
+    /* 문제가 2022년 기준이라 이 코드는 에러처리가 됨
+    return new Date().getFullYear() - age + 1;
+    */
 }
