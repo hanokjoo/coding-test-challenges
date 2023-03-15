538107dd0614ba2e406fef0f6160d559f19fa301
Okjoo Han <okjoo.han88@gmail.com>
2023-03-15 22:58:37 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -4,4 +4,16 @@ function solution(n) {
         if (i % 2 === 0) answer += i;
     }
     return answer;
+    
+    /* 참고할 만한 코드
+    - 수열 공식 활용
+    var half = Math.floor(n/2);
+    return half*(half+1);
+    
+    - 반복 횟수 줄임
+    var answer = 0;
+    for(let i=2 ; i<=n ; i+=2)
+        answer += i;
+    return answer;
+    */
 }
