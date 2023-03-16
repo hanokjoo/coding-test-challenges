a53d3923fc35f53c77c8fc9d14f07533767e90bf
Okjoo Han <okjoo.han88@gmail.com>
2023-03-16 23:45:37 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,3 +1,9 @@
 function solution(array, height) {
     return array.filter((e) => e > height).length;
+    
+    /* 참고할 만한 코드 - map 활용
+    let result = 0;
+    array.map((arr) => height < arr && result++);
+    return result;
+    */
 }
