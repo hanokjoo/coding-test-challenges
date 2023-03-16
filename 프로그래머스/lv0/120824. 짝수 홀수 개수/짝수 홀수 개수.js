7759c96752b022bee59adb462fef657c20d46226
Okjoo Han <okjoo.han88@gmail.com>
2023-03-16 23:55:48 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,4 +1,18 @@
 function solution(num_list) {
     let even = num_list.filter((e) => e % 2 === 0).length;
     return [even, num_list.length - even];
+    
+    /* 참고할 만한 코드
+    - 배열의 주소 활용
+    var answer = [0,0];
+    
+    for(let a of num_list){
+        answer[a%2] += 1
+    }
+    
+    return answer;
+    
+    - reduce 활용
+    return list.reduce((acc, cur) => (cur & 1 ? acc[1]++ : acc[0]++, acc), [0, 0])
+    */
 }
