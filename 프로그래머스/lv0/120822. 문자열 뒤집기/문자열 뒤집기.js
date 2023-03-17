868357b2f1b17b2b9dc2b825884b96b9798f5c00
Okjoo Han <okjoo.han88@gmail.com>
2023-03-17 21:32:51 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -2,4 +2,8 @@ function solution(my_string) {
     let temp = [];
     [...my_string].forEach((e) => temp.unshift(e));
     return temp.join("");
+    
+    /* 참고할 만한 코드 - reverse() 사용
+    return [...my_string].reverse().join("");
+    */
 }
