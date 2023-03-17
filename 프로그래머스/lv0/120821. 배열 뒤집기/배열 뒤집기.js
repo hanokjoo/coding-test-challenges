fae8a17c68fac65cf61415b7427771ee6eeb8ecd
Okjoo Han <okjoo.han88@gmail.com>
2023-03-17 21:24:53 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -2,4 +2,8 @@ function solution(num_list) {
     var answer = [];
     num_list.forEach((e) => answer.unshift(e));
     return answer;
+    
+    /* 참고할 만한 코드 - reverse() 사용
+    return num_list.reverse();
+    */
 }
