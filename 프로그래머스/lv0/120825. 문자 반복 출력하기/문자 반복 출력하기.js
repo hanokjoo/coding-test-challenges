4762a862b7a7aed8f562e721585c106ba709c26f
Okjoo Han <okjoo.han88@gmail.com>
2023-03-18 23:43:36 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -4,4 +4,9 @@ function solution(my_string, n) {
        for (let i = 0; i < n; i++) answer.push(e);
     });
     return answer.join("");
+    
+    /* 참고할 만한 코드 - String.repeat() 사용
+    let answer = [...my_string].map(v => v.repeat(n)).join("");
+    return answer;
+    */
 }
