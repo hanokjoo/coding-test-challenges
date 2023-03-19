7e92a32d2b3ae58a19a11d8382730b2754cded70
Okjoo Han <okjoo.han88@gmail.com>
2023-03-19 23:33:18 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,15 @@
+function solution(hp) {
+    let answer = 0, remain = hp;
+    if (hp >= 5) {
+        answer += Math.floor(hp / 5);
+        remain = hp % 5;
+    }
+    if (remain >= 3) {
+        answer += Math.floor(remain / 3);
+        remain = remain % 3;
+    }
+    if (remain > 0) {
+        answer += remain;
+    }
+    return answer;
+}
