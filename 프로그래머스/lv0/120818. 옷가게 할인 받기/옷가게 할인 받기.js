bc5fa5805c7ee131497ab0393b8fb3aabf1e5e58
Okjoo Han <okjoo.han88@gmail.com>
2023-03-19 23:24:51 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -3,4 +3,13 @@ function solution(price) {
     else if (price < 500000 && price >= 300000) return Math.floor(price * 0.9);
     else if (price < 300000 && price >= 100000) return Math.floor(price * 0.95);
     else return price;
+    
+    /* 참고할 만한 코드 - 반복문 활용
+    let arr_discount = [[500000, 0.2], [300000, 0.1], [100000, 0.05]];
+
+    for(let i = 0 ; i < arr_discount.length ; i++)
+        if(arr_discount[i][0] <= price) return Math.trunc(price * (1-arr_discount[i][1]));      
+
+    return price;
+    */
 }
