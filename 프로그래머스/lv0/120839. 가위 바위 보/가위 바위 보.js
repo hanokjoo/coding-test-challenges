142e4c26fd4d28b5e472529d3a4527a599aa11cd
Okjoo Han <okjoo.han88@gmail.com>
2023-03-19 23:40:19 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -4,4 +4,14 @@ function solution(rsp) {
         else if (e === "2") return 0;
         else if (e === "5") return 2;
     }).join("");
+    
+    /* 참고할 만한 코드
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    let answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+    */
 }
