ef5b8162f3323a4bec8682dee42e70c05b567d9e
Okjoo Han <okjoo.han88@gmail.com>
2023-03-20 21:37:01 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,4 +2,12 @@ function solution(age) {
     return [...age.toString()].map((e) => {
         return String.fromCharCode(97 + Number(e));
     }).join("");
+    
+    /* 참고할 만한 코드 - 문자열 자체의 인덱스 활용
+    return age
+    .toString()
+    .split("")
+    .map((v) => "abcdefghij"[v])
+    .join("");
+    */
 }
