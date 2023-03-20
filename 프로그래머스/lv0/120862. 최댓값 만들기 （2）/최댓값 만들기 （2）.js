5f90e2697bf673108fd02964ee2700f70a12c983
Okjoo Han <okjoo.han88@gmail.com>
2023-03-20 21:10:34 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -14,4 +14,12 @@ function solution(numbers) {
             return numbers[numbers.length - 1] * numbers[numbers.length - 2];
         }
     }  
+    
+    /* 참고할 만한 코드 - Math.max() 사용
+    numbers.sort((a, b) => a - b);
+    return Math.max(
+        numbers[0]*numbers[1], 
+        numbers[numbers.length-1]*numbers[numbers.length-2]
+    );
+    */
 }
