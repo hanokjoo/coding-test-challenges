1c78ecb5db7ac2be21ac562f648618a926f2ebaa
Okjoo Han <okjoo.han88@gmail.com>
2023-03-23 22:29:08 +0900

    [level 0] Title: 유한소수 판별하기, Time: 2.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -19,3 +19,16 @@ function solution(a, b) {
 function getGCD(a, b) {
     return (a % b === 0) ? b : getGCD(b, a % b);
 }
+
+/* 참고할 만한 코드
+    let n = 1;
+    for (let i = 1; i <= Math.min(a, b); i++) {
+        if (a % i === 0 && b % i === 0) n = i;
+    }
+
+    b /= n;
+    while (b % 2 === 0) b /= 2;
+    while (b % 5 === 0) b /= 5;
+
+    return b === 1 ? 1 : 2;   
+*/
