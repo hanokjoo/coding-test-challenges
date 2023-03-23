2ced4338b594b79fd6edb69469ec6072b22110c2
Okjoo Han <okjoo.han88@gmail.com>
2023-03-23 13:36:57 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -4,4 +4,12 @@ function solution(array) {
     let max = Math.max(...count); 
     if (count.filter((e) => e === max).length > 1) return -1;
     else return count.findIndex(e => e === max);
+    
+    /* 참고할 만한 코드
+    - map활용해서 카운트
+    let m = new Map();
+    for (let n of array) m.set(n, (m.get(n) || 0)+1);
+    m = [...m].sort((a,b)=>b[1]-a[1]);
+    return m.length === 1 || m[0][1] > m[1][1] ? m[0][0] : -1;
+    */
 }
