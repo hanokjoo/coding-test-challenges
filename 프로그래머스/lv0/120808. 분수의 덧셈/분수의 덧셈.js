fef7d043ac7f156a46d4d1d39fbbfb6ba5518db2
Okjoo Han <okjoo.han88@gmail.com>
2023-03-23 21:53:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.49 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -9,4 +9,18 @@ function solution(numer1, denom1, numer2, denom2) {
         } else div++;
     }
     return [newNumer, newDenom];
+    
+    /* 참고할 만한 코드 - 최대공약수 구해서 나누기
+    function fnGCD(a, b){
+        return (a % b) ? fnGCD(b, a % b) : b;
+    }
+
+    function solution(denum1, num1, denum2, num2) {
+        let denum = denum1 * num2 + denum2 * num1;
+        let num = num1 * num2;
+        let gcd = fnGCD(denum, num); //최대공약수
+
+        return [denum / gcd, num / gcd];
+    }
+    */
 }
