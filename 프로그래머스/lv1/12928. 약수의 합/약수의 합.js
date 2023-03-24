4423be65a95c13c0aad2b46d067389ff3f8896d5
Okjoo Han <okjoo.han88@gmail.com>
2023-03-24 23:12:04 +0900

    [level 1] Title: 약수의 합, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -10,4 +10,25 @@ function solution(n) {
     divisor.forEach((e, idx, thisArr) => thisArr.push(n / e));
     
     return [...new Set(divisor)].reduce((acc, cur) => acc + cur, 0);
+    
+    /* 참고할 만한 코드
+    - 1씩 증가하면 다 나눠보기
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) sum += i;
+    }
+    return sum;
+    
+    - 짝을 바로 더하기
+    var answer = 0;
+    let i;
+    for (i = 1; i <= Math.sqrt(n); i++){
+        if (!(n % i)) {
+            answer += (i + n / i);
+        }
+    }
+    i--;
+    return (i === n / i) ? answer-i : answer;
+    */
+    
 }
