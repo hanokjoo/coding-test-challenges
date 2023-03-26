814c848f132b2627d0b7d9c0cd0a5abfec0e06a0
Okjoo Han <okjoo.han88@gmail.com>
2023-03-26 23:38:49 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.23 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,6 +1,7 @@
 function solution(a, b) {
     if (a === b) return a;
     if (a + b === 0) return 0;
+    // sort() 시 마이너스 유의: x - y로 하면 [-1, -100]로 정렬된다.
     let arr = [a, b].sort((x, y) => Number(x) - Number(y));
     let answer = 0;   
     for (let i = arr[0]; i <= arr[1]; i++) {
