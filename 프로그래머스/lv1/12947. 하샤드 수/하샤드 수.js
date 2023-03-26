d0aeb7b3e67faaa18566e908e95d7dd19a3789b0
Okjoo Han <okjoo.han88@gmail.com>
2023-03-26 23:02:57 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,3 +1,14 @@
 function solution(x) {
     return (x % Array.from(x + "").reduce((acc, cur) => acc + Number(cur), 0) === 0);
+    
+    /* 참고할 만한 코드 - 숫자로 접근해서 풀이, 속도는 더 빠르다고 함
+    let num = x;
+    let sum = 0;
+    do {
+        sum += x % 10;
+        x = Math.floor(x / 10);
+    } while (x > 0);
+
+    return !(num % sum);
+    */
 }
