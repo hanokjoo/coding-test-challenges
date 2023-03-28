c9dc2d546ca82d92bdbddf0d1ced2fa83547c32b
Okjoo Han <okjoo.han88@gmail.com>
2023-03-28 22:41:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -6,4 +6,10 @@ function solution(arr) {
         if (e < found) found = e;
     });
     return arr.filter((e) => e > found);
+    
+    /* 참고할 만한 코드 - Math.min() 사용
+    if (arr.length <= 1) return [-1];
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    return arr;
+    */
 }
