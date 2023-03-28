b77297a30f246f02930743c81dae0aaf8e11b370
Okjoo Han <okjoo.han88@gmail.com>
2023-03-28 22:54:30 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,4 +1,9 @@
 function solution(s) {
     if (s.length % 2 === 0) return s.charAt(s.length / 2 - 1) + s.charAt(s.length / 2);
     else return s.charAt(Math.floor(s.length / 2));
+    
+    /* 참고할 만한 코드 - 문자열도 배열처럼 접근 가능
+    const mid = Math.floor(s.length / 2);
+    return s.length % 2 === 1 ? s[mid] : s[mid - 1] + s[mid];
+    */
 }
