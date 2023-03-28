514dc0d32c0b4aaf9b08f757182e14d7a888e17c
Okjoo Han <okjoo.han88@gmail.com>
2023-03-28 22:44:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -4,4 +4,8 @@ function solution(numbers) {
         if (numbers.indexOf(i) === -1) answer += i;
     }
     return answer;
+    
+    /* 참고할 만한 코드 - "전체합 - 배열합"으로 풀이
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+    */
 }
