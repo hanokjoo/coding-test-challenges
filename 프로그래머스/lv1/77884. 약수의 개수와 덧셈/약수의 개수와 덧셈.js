9ab6632962a168448c132d35872aba2995b492cb
Okjoo Han <okjoo.han88@gmail.com>
2023-03-29 23:22:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -21,3 +21,17 @@ function getDivCount(n) {
     
     return divisors.length;
 }
+
+/* 참고할 만한 코드 - 제곱근이 정수면 약수의 갯수는 홀수이다.
+function solution (left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
+*/
