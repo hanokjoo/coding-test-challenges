6b7749bde695271909d1143c560b91f93cc4a982
Okjoo Han <okjoo.han88@gmail.com>
2023-03-29 22:53:42 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -4,4 +4,10 @@ function solution(a, b) {
         answer += e * b[idx];
     });
     return answer;
+    
+    /* 참고할 만한 코드 - reduce() 사용
+    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+    
+    (사용 안하는 arg에 _를 쓴다.)
+    */
 }
