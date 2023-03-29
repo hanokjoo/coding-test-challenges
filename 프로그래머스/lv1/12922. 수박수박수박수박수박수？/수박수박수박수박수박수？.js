7802551338bb2cc5f007333eaa2701b2260f15e6
Okjoo Han <okjoo.han88@gmail.com>
2023-03-29 22:48:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,3 +1,7 @@
 function solution(n) {
     return new Array(n).fill("").map((e, idx) => (idx % 2 === 0) ? "수" : "박").join(""); 
+    
+    /* 참고할 만한 코드 - repeat() 사용
+    return "수박".repeat(n).slice(0, n);
+    */
 }
