04dfee2dae3f73a6e9f67513e12380b2dbe76902
Okjoo Han <okjoo.han88@gmail.com>
2023-03-29 23:33:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,9 @@
 function solution(s) {
     return ((s.length === 4 || s.length === 6)
             && (s.replace(/[0-9]/g, "").length === 0));
+    
+    /* 참고할 만한 코드 - 정규식 더 적극 활용하기
+    var regex = /^\d{6}$|^\d{4}$/;
+    return regex.test(s);
+    */
 }
