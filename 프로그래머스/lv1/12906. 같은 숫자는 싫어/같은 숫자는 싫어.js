903774d356c3f61ed5ae36eff526226e872eb6df
Okjoo Han <okjoo.han88@gmail.com>
2023-03-30 23:24:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.97 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,8 @@
 function solution(arr)
 {
-    if (arr.every((e) => e === arr[0])) return [arr[0]];
-    
     let answer = [];
     
     for (let i = 0; i < arr.length; i++) {
-        //if (i === arr.length - 1 && arr[i - 1] !== arr[i]) answer.push(arr[i]);
         if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
     }
     return answer;
@@ -14,5 +11,9 @@ function solution(arr)
     let answer = [];   
     arr.join("").replace(/0+|1+|2+|3+|4+|5+|6+|7+|8+|9+/g, (str) => answer.push(str[0] * 1));   
     return answer;
+    */
+    
+    /* 참고할 만한 코드 - filter() 사용
+    return arr.filter((val,index) => val != arr[index + 1]);
     */
 }
