12591520658e9fcd63f0adba20bfafc1e53ee0db
Okjoo Han <okjoo.han88@gmail.com>
2023-04-01 23:30:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -12,5 +12,13 @@ function solution(s) {
     }
         
     return answer;
+    
+    /* 참고할 만한 코드
+    - 정규식 사용
+    return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();});
+    
+    - split(), join() 사용
+    return s.split(' ').map(i => i.split('').map((j, key) => key % 2 === 0 ? j.toUpperCase() : j.toLowerCase()).join('')).join(' ');
+    */
                             
 }
