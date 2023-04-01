1b490e53618e7131f4b980d026b668f9c5f4cb56
Okjoo Han <okjoo.han88@gmail.com>
2023-04-02 00:18:59 +0900

    [level 1] Title: 시저 암호, Time: 49.96 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,10 +1,4 @@
 function solution(s, n) {
-    // const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-    //                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
-    //                "u", "v", "w", "x", "y", "z", "a"];
-    // const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-    //                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-    //                "U", "V", "W", "X", "Y", "Z", "A"];
     const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lower = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
     
@@ -24,20 +18,10 @@ function solution(s, n) {
         }
     }
     
-    // for (let i = 0; i < s.length; i++) {
-    //     codeValue = s.charCodeAt(i);
-    //     if (codeValue >= 65 && codeValue < 90) {
-    //         answer += String.fromCharCode(codeValue + 1);
-    //     } else if (codeValue >= 97 || codeValue < 122) {
-    //         answer += String.fromCharCode(codeValue + 1);
-    //     } else if (codeValue === 90) {
-    //         answer += "A";
-    //     } else if (codeValue === 122) {
-    //         answer += "a";
-    //     } else {
-    //         answer += " ";
-    //     }
-    // }
-    
     return answer;
+    
+    /* 참고할 만한 코드 - 대소문자 통합해서 풀이
+    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+    return s.split('').map(e => chars[chars.indexOf(e) + n]).join('');
+    */
 }
