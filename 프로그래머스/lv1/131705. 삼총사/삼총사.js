920fc60b57986a1d36122cd6da6625de0c304b49
Okjoo Han <okjoo.han88@gmail.com>
2023-04-03 23:14:25 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,12 +1,10 @@
 function solution(number) {
     let count = 0;
     
-    const getSum = (i, j, k) => number[i] + number[j] + number[k];
-    
     for (let i = 0; i < number.length - 2; i ++) {
         for (let j = i + 1; j < number.length - 1; j++) {
             for (let k = j + 1; k < number.length; k++) {
-                if (getSum(i, j, k) === 0) {     
+                if (number[i] + number[j] + number[k] === 0) {     
                     count++;
                 }
             }
