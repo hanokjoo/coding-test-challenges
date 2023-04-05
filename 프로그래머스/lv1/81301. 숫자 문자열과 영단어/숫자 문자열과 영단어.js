7097355b351a101b5e47187d6ced9dddcccac4b2
Okjoo Han <okjoo.han88@gmail.com>
2023-04-05 23:59:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -10,4 +10,11 @@ function solution(s) {
     });
     
     return s * 1;
+    
+    /* 참고할 만한 코드 - 배열의 인덱스 활용, reduce() 사용
+    const digit2word = ['zero','one','two','three','four','five','six','seven', 'eight','nine'];
+    function solution(s) {
+        return Number(digit2word.reduce((ans, word, digit) => ans.replace(new RegExp(word, 'g'), digit), s));
+    }
+    */
 }
