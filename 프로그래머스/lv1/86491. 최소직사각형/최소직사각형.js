d652724c1e5268719a7693b31ee377ab1a183f2b
Okjoo Han <okjoo.han88@gmail.com>
2023-04-06 23:41:59 +0900

    [level 1] Title: 최소직사각형, Time: 1.98 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,5 +1,5 @@
 function solution(sizes) {
-    let horMax = 0; verMax = 0;
+    let horMax = 0, verMax = 0;
     sizes.forEach((e) => {
         if (e[0] >= e[1]) {
             if (horMax < e[0]) horMax = e[0];
@@ -11,4 +11,15 @@ function solution(sizes) {
     });
     
     return horMax * verMax;
+    
+    /* 참고할 만한 코드 - map()으로 가로 세로 바꾸기
+    const rotated = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]);
+
+    let maxSize = [0, 0];
+    rotated.forEach(([w, h]) => {
+        if (w > maxSize[0]) maxSize[0] = w;
+        if (h > maxSize[1]) maxSize[1] = h;
+    });
+    return maxSize[0]*maxSize[1];
+    */
 }
