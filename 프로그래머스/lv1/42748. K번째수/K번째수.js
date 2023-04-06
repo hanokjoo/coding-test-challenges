a60ddcceadb257374f70626d07bf81c942a556bc
Okjoo Han <okjoo.han88@gmail.com>
2023-04-06 23:52:21 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -4,4 +4,14 @@ function solution(array, commands) {
         answer.push(array.slice(i - 1, j).sort((x, y) => x - y)[k - 1]);
     });   
     return answer;
+    
+    /* 참고할 만한 코드 - map() 사용, 구조분해할당
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command;
+        const newArray = array
+            .slice(sPosition - 1, ePosition)
+            .sort((a,b) => a - b);
+        return newArray[position - 1];
+    });
+    */
 }
