d9034a2d76afaa7253ad28b282e85945226da031
Okjoo Han <okjoo.han88@gmail.com>
2023-04-07 22:03:30 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -6,4 +6,23 @@ function solution(food) {
         }
     }
     return half.join("") + "0" + half.reverse().join(""); 
+    
+    /* 참고할 만한 코드
+    - 직관적임
+    let player1 = [];
+    let player2 = [];
+    let repeat = 0;
+    for (let i = 1; i <= food.length; i++) {
+        if (food[i] > 1) {
+            repeat = Math.floor(food[i] / 2);
+            for (let j = 0; j < repeat; j++)
+                player1.push(i)
+            for (let k = 0; k < repeat; k++)
+                player2.unshift(i)
+        }
+    }
+    player1.push(0);
+    player1 = player1.concat(player2);
+    return player1.join("");
+    */
 }
