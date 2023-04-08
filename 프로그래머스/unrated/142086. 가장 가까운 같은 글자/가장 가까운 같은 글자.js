f8f25a0b465c5066fa3ecfe324b58112a46dbc72
Okjoo Han <okjoo.han88@gmail.com>
2023-04-08 23:46:56 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 40.86 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -11,4 +11,11 @@ function solution(s) {
     });
     
     return answer;
+    
+    /* 참고할 만한 코드
+    [...s].map((char, i) => {
+        const count = s.slice(0, i).lastIndexOf(char);
+        return count < 0 ? count : i - count;
+      });
+    */
 }
