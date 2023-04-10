990ecacb42b0e74db85e3d5b4f682b0284640750
Okjoo Han <okjoo.han88@gmail.com>
2023-04-10 22:29:41 +0900

    [level 1] Title: 폰켓몬, Time: 0.32 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,4 @@
 function solution(nums) {
     const kind = new Set(nums).size;
-    return (kind <= Math.floor(nums.length / 2)) ? kind : Math.floor(nums.length / 2);
+    return Math.min(kind, Math.floor(nums.length / 2));
 }
