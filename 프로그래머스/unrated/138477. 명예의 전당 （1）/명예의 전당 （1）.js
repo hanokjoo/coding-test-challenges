73595cccd34108e17636f9189421c630dafdc63e
Okjoo Han <okjoo.han88@gmail.com>
2023-04-11 21:27:18 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -12,4 +12,21 @@ function solution(k, score) {
         }
     });
     return answer;
+    
+    /* 참고할 만한 코드 - reduce()와 push, shift로 풀이
+    const stack = [];
+    return score.reduce((a,c) => {
+        if(stack.length < k) {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+        }
+        else {
+            stack.push(c)
+            stack.sort((a,b) => a - b)
+            stack.shift()
+        }
+        a.push(stack[0])
+        return a
+    },[]);
+    */
 }
