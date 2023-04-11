5fdcff2da27828427b5ff26087dc8edae8e84d29
Okjoo Han <okjoo.han88@gmail.com>
2023-04-11 21:16:08 +0900

    [unrated] Title: 과일 장수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -8,7 +8,6 @@ function solution(k, m, score) {
     let scoreIdx = classify.length - 1;
     let perBox = m;
     while (scoreIdx >= 0) {
-        //console.log(scoreidx, )
         if (classify[scoreIdx] > perBox) {
             classify[scoreIdx] -= perBox;
             answer += (scoreIdx + 1) * m;
@@ -23,4 +22,13 @@ function solution(k, m, score) {
         } 
     }
     return answer;
+    
+    /* 참고할 만한 코드 - 정렬하고 남는 수 버린 후 박스단위로 for문 증가하면서 최소값만 뽑아서 더한다.
+    let answer = 0;
+    const sortedScore = score.slice().sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
+    }
+    return answer;
+    */
 }
