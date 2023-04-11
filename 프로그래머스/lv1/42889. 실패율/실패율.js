a71a3ada1de168d975efee71570307d8b427b994
Okjoo Han <okjoo.han88@gmail.com>
2023-04-11 22:03:50 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -15,7 +15,6 @@ function solution(N, stages) {
     }
     
     answer.pop();
-    //console.log(answer);
     return answer.sort((a, b) => {
         if (b[1] === a[1]) return a[0] - b[0];
         else return b[1] - a[1]
@@ -23,4 +22,15 @@ function solution(N, stages) {
         a.push(c[0]);
         return a;
     }, []);
+    
+    /* 참고할 만한 코드 - 코드는 간결하나 성능은 느림
+    let result = [];
+    for(let i = 1; i <= N; i++) {
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr / reach]);
+    }
+    result.sort((a, b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
+    */
 }
