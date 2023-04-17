d673f087b2d4fa23c63eacceed82cfef1be1f9ff
Okjoo Han <okjoo.han88@gmail.com>
2023-04-17 23:27:50 +0900

    [level 1] Title: 체육복, Time: 2.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -24,4 +24,30 @@ function solution(n, lost, reserve) {
     } 
 
     return n - lost.filter(e => e > 0).length;
+    
+    /* 참고할 만한 코드
+    const students = {};
+    let answer = 0;
+    for (let i = 1; i <= n; i++) {
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
+
+    for (let i = 1; i <= n; i++) {
+        if (students[i] === 2 && students[i-1] === 0) {
+                students[i-1]++;
+                students[i]--;
+        } else if (students[i] === 2 && students[i+1] === 0) {
+                students[i+1]++;
+                students[i]--;
+        }
+    }
+    for (let key in students) {
+        if (students[key] >= 1) {
+            answer++;
+        }
+    }
+    return answer;
+    */
 }
