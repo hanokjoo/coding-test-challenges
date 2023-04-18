e5423671b14044d2bf14ae5ebea61c0d8e7170af
Okjoo Han <okjoo.han88@gmail.com>
2023-04-18 23:06:23 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -2,17 +2,26 @@ function solution(new_id) {
     new_id = new_id.toLowerCase()
         .replace(/[^a-z0-9\-\_\.]/g, "")
         .replace(/\.{2,}/g, ".");
-    console.log(new_id);
     if (new_id.startsWith(".")) new_id = new_id.slice(1);
     if (new_id.endsWith(".")) new_id = new_id.slice(0, new_id.length - 1);
-    console.log(new_id);
     if (new_id.length === 0) new_id = "a";
     if (new_id.length > 15) new_id = new_id.slice(0, 15);
-    console.log(new_id);
     if (new_id.endsWith(".")) new_id = new_id.slice(0, new_id.length - 1);
     if (new_id.length < 3) {
         while(new_id.length < 3) new_id += new_id.slice(-1);
     }
     
     return new_id;
+    
+    /* 참고할 만한 코드 - 정규식 활용, padEnd()
+    const id = new_id
+        .toLowerCase()
+        .replace(/[^\w\d-_.]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .padEnd(1, 'a')
+        .slice(0, 15)
+        .replace(/^\.|\.$/g, '');        
+    return id.padEnd(3, id[id.length-1]);
+    */
 }
