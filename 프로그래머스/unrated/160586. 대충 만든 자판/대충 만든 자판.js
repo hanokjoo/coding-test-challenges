63a86ed65c2304f043cf64bac97e4dd00832320a
Okjoo Han <okjoo.han88@gmail.com>
2023-04-24 23:29:20 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.84 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -27,4 +27,16 @@ function solution(keymap, targets) {
     });
     
     return targets;
+    
+    /* 참고할 만한 코드 - keymap에 있는 문자열만 최소횟수 구하고 targets 돌면서 횟수 더해서 push로 답 완성 
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
+    }
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
+    }
+    return answer;
+    */
 }
