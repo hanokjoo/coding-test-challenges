70a29902affcad98d2e3eb6d65db817b7db1080a
Okjoo Han <okjoo.han88@gmail.com>
2023-04-24 22:11:47 +0900

    [unrated] Title: 둘만의 암호, Time: 0.26 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -9,4 +9,11 @@ function solution(s, skip, index) {
         filtered = filtered.concat(filterSkip);
     }
     return [...s].map(e => filtered[filtered.findIndex(s => s === e) + index]).join("");
+    
+    /* 참고할 만한 코드 - filterSkip의 길이의 나머지로 index를 찾는다. alphabet 필터링 시 정규식 사용도 좋음.
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                      "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+                      "u", "v", "w", "x", "y", "z"].filter(c => !skip.includes(c));
+    return s.split("").map(c => alphabet[(alphabet.indexOf(c) + index) % alphabet.length]).join("");
+    */
 }
