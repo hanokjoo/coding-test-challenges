5e9564de950181da0a6e3ec3ccab688d2646faa9
Okjoo Han <okjoo.han88@gmail.com>
2023-05-09 23:04:53 +0900

    [level 1] Title: 소수 찾기, Time: 31.93 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -10,4 +10,22 @@ function solution(n) {
     }
     
     return numbers.filter(e => e === 0).length - 2;
+    
+    /* 참고할 만한 코드
+    - set 활용해서 풀이
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
+    }
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
+        }
+    }
+    return s.size;
+    */
 }
