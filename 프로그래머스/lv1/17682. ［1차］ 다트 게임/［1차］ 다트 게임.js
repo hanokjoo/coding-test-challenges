14b1d0f02bc5266bb60eddfb68f2e6d928cd7875
Okjoo Han <okjoo.han88@gmail.com>
2023-05-12 14:58:39 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -25,5 +25,22 @@ function solution(dartResult) {
     
     return score.reduce((a, c) =>  a + c, 0);
     
+    /* 참고할 만한 코드 - 다른 정규식 활용
+      const bonus = {'S': 1, 'D': 2, 'T': 3},
+          options = {'*': 2, '#': -1, '': 1};
+
+    let darts = dartResult.match(/\d{1,}[SDT][*#]?/g);
+
+    for (let i = 0; i < darts.length; i++) {
+        let split = darts[i].match(/(\d{1,})([SDT])([*#]?)/),
+            score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+        if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+
+        darts[i] = score;
+    }
+
+    return darts.reduce((a, b) => a + b);
+    */
     
 }
