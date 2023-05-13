060316bc4af9870930744bbe59338dea0dc48cb2
Okjoo Han <okjoo.han88@gmail.com>
2023-05-13 21:34:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -11,4 +11,15 @@ function solution(lottos, win_nums) {
 
     if (see.length === 6 && min === 0) return [6, 6];
     else return [(min === 0) ? 1 : 7 - min - (6 - see.length), (min === 0) ? 6 : 7 - min];
+    
+    /* 참고할 만한 코드 - rank 설정방식, !0가 true임을 활용, includes로 찾기는 시간 많이 걸릴 수 있음
+    const rank = [6, 6, 5, 4, 3, 2, 1];
+
+    let minCount = lottos.filter(v => win_nums.includes(v)).length;
+    let zeroCount = lottos.filter(v => !v).length;
+
+    const maxCount = minCount + zeroCount;
+
+    return [rank[maxCount], rank[minCount]];
+    */
 }
