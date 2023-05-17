6979c5427335803589247244cd97598a26e5711c
Okjoo Han <okjoo.han88@gmail.com>
2023-05-18 00:08:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -22,4 +22,20 @@ function solution(id_list, report, k) {
         if (mailCnt.get(e)) return mailCnt.get(e);
         else return 0;
     });
+    
+    /* 참고할 만한 코드 - 처음부터 set으로 중복신고 제거함
+    let reports = [...new Set(report)].map(a => {return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports) {
+        counts.set(bad[1], counts.get(bad[1]) + 1 || 1);
+    }
+    let good = new Map();
+    for(const report of reports) {
+        if(counts.get(report[1]) >= k) {
+            good.set(report[0], good.get(report[0]) + 1 || 1);
+        }
+    }
+    let answer = id_list.map(a => good.get(a) || 0);
+    return answer;
+    */
 }
