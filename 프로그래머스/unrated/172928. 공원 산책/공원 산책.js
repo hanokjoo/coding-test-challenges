58056cc0fa8bdbcf9b7ada7e52e5a2900f93af97
Okjoo Han <okjoo.han88@gmail.com>
2023-05-21 23:02:48 +0900

    [unrated] Title: 공원 산책, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -58,4 +58,28 @@ function solution(park, routes) {
     }
     
     return now;
+    
+    /* 참고할 만한 코드 - 방향 Object를 만들어서 현재 좌표에서 더해주는 방식으로 체크 후 이동
+    const dirs = { E: [0, 1], W: [0, -1], S: [1, 0], N: [-1, 0] };
+        let [x, y] = [0, 0];
+        for (let i = 0; i < park.length; i++) {
+            if (park[i].includes('S')) {
+                [x, y] = [i, park[i].indexOf('S')];
+                break;
+            }
+        }
+
+        routes.forEach((route) => {
+            const [r, n] = route.split(' ');
+            let [nx, ny] = [x, y];
+            let cnt = 0;
+            while (cnt < n) {
+                [nx, ny] = [nx + dirs[r][0], ny + dirs[r][1]];
+                if (!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') break;
+                cnt++;
+            }
+            if (cnt == n) [x, y] = [nx, ny];
+        });
+        return [x, y];
+        */
 }
