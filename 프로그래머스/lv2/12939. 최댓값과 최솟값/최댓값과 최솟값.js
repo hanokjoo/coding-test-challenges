888dbc7472057940987343c4acc6658c19320b25
Okjoo Han <okjoo.han88@gmail.com>
2023-05-22 23:40:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,4 +2,9 @@ function solution(s) {
     const arr = s.split(" ");
     arr.sort((a, b) => a - b);
     return arr[0] + " " + arr[arr.length - 1];
+    
+    /* 참고할 만한 코드 - Math 메소드 활용
+    const arr = s.split(' ');
+    return Math.min(...arr) + ' ' + Math.max(...arr);
+    */
 }
