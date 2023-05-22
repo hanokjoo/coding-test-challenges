1d462b626f499d14c525494e9dcfc7522ddeb816
Okjoo Han <okjoo.han88@gmail.com>
2023-05-23 00:22:01 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -20,8 +20,10 @@ function solution(s) {
     
     /* 참고할 만한 코드
     1. 공백 여러개를 하나로 치환 후 배열로 쪼개기
-    2. charAt()으로 접근하여 처리
-    (str[index]로 접근하는 것 보다 나은 이유: https://thisthat.dev/string-char-at-vs-string-bracket-notation/)
+    2. v[0] 또는 v.charAt(0)으로 접근하여 toUpperCase() 처리
+    -> 1. 과정없이 split하면 연속공백자리에 ""문자열인 v가 생기는데 이걸 v[0]로 접근하면 undefined에 toUpperCase()할 수 없다고 에러나옴.
+    -> 1. 과정 없이 하려면 v.charAt(0)로 접근하면 에러없이 ""로 넘어간다. 그러나 제출하는 답안에도 연속공백이 생기므로 
+    -> 1. 과정 수행 후 대소문자 처리를 해주는 것이 좋다.
     
     return s.replace(/\s+/g, " ")
         .split(" ")
