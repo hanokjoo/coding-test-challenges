ba8aea131edc4c13aaaa4eeba6fcda531a73b4be
Okjoo Han <okjoo.han88@gmail.com>
2023-05-23 00:16:02 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -17,4 +17,15 @@ function solution(s) {
         newStr = "";
     });
     return answer.join(" ");
+    
+    /* 참고할 만한 코드
+    1. 공백 여러개를 하나로 치환 후 배열로 쪼개기
+    2. charAt()으로 접근하여 처리
+    (str[index]로 접근하는 것 보다 나은 이유: https://thisthat.dev/string-char-at-vs-string-bracket-notation/)
+    
+    return s.replace(/\s+/g, " ")
+        .split(" ")
+        .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
+        .join(" ");
+    */
 }
