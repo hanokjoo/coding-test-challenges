0db99514d2864839def6f34dea4d86a0eb9051fa
Okjoo Han <okjoo.han88@gmail.com>
2023-05-25 23:35:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.07 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -6,20 +6,22 @@ function solution(s)
         if (stack[stack.length - 1] === s[i]) stack.pop();
         else stack.push(s[i]);
     }
-    //console.log(stack);
+
     return stack.length > 0 ? 0 : 1;
     
     /* 이 방식은 시간초과가 나온다.
-    if (s.length < 2) return 0;
-    let first = s.substring(0, Math.floor(s.length / 2));
-    let second = s.substring(Math.floor(s.length / 2));
-    
-    first = removeRepeatChar(first);
-    second = removeRepeatChar(second);    
-    
-    let third = first + second;
-    third = removeRepeatChar(third);       
-    return third.length > 0 ? 0 : 1;
+    function solution(s) {
+        if (s.length < 2) return 0;
+        let first = s.substring(0, Math.floor(s.length / 2));
+        let second = s.substring(Math.floor(s.length / 2));
+
+        first = removeRepeatChar(first);
+        second = removeRepeatChar(second);    
+
+        let third = first + second;
+        third = removeRepeatChar(third);       
+        return third.length > 0 ? 0 : 1;
+    }
     
     function removeRepeatChar(str) {
         let oldStrLength = 0;
