5d57eaf6125f514c17ecac197c113dd8996d89e2
Okjoo Han <okjoo.han88@gmail.com>
2023-05-29 22:26:03 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -5,4 +5,19 @@ function solution(brown, yellow) {
         }
     }
     return [0, 0];
+    
+    /*
+    array[i][j] 라 할 때,
+    i * 2 + (j - 2) * 2
+    -> (i + j - 2) * 2 = 브라운갯수
+    -> i + j - 2 = brown / 2
+    -> i + j = bround / 2 + 2
+    */
+    
+    /* 참고할 만한 코드 
+    let xpy = brown / 2 + 2;
+    for (let a = 1; a < xpy; a++) {
+        if (a * (xpy - a) === brown + yellow) {
+            return [Math.max(a, xpy - a), Math.min(a, xpy - a)];
+    */
 }
