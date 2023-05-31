e6b7e038fc0b6413790c26d63c889cd58560931c
Okjoo Han <okjoo.han88@gmail.com>
2023-05-31 15:43:50 +0900

    [level 2] Title: 구명보트, Time: 11.79 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -4,21 +4,18 @@ function solution(people, limit) {
     
     while(people.length > 1) {
         if (people[0] + people[people.length - 1] <= limit) {
-            //console.log("1:", people[0], people[people.length - 1]);
             answer++;
             people.shift();
             people.pop();
         } else {
-            //console.log("2:", people[0], people[people.length - 1]);
             answer++;
             people.pop();
         }
     }
-    //console.log("3:", people);
     return people.length + answer;
     
     
-    /* 시간초과만 걸림
+    /* 테스트케이스는 모두 통과, 시간초과만 걸림
     let findIdx = -1, findPair = false;
     for (let i = 0; i < people.length; i++) {
         if (people[i] > 0) {
@@ -41,34 +38,13 @@ function solution(people, limit) {
     return people.filter(e => e > -1).length + answer;
     */
     
-    /*const half = Math.floor(limit / 2);
-    const light = [], middle = [], heavy = [];
-    people.forEach(e => {
-        if (e < half) light.push(e);
-        else if (e === half) middle.push(e);
-        else if (e > half) heavy.push(e);
-    });
-    
-    light.sort();
-    heavy.sort((a, b) => b - a);
-    console.log("l:", light);
-    console.log("m:", middle);
-    console.log("h:", heavy);
-    
-    let answer = 0;
-    let hIndex = -1, findArr = (light.length > heavy.length) ? [light, heavy] : [heavy, light];
-    for(let i = 0; i < findArr[0].length; i++) {
-        hIndex = findArr[1].findIndex(e => e === limit - findArr[0][i]);
-        if (hIndex > -1) {
-            findArr[0][i] = 0;
-            findArr[1][hIndex] = 0;
-            answer++;
-        }
-    }
-    
-    console.log("answer:", answer);
-    console.log("l:", light);
-    console.log("m:", middle);
-    console.log("h:", heavy);
+    /* 참고할 만한 코드 
+    - var로 선언한 i의 호이스팅으로 반복문 밖에서도 사용 가능.
+    - let으로 바꾸고 cnt 하나 생성해도 좋을 듯.
+    people.sort(function (a, b) {return a-b});
+    for (var i = 0, j = people.length - 1; i < j; j--) {
+        if( people[i] + people[j] <= limit ) i++;
+    }    
+    return people.length - i;
     */
 }
