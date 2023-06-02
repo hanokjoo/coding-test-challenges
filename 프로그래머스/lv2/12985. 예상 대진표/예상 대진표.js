e6014d8014065e124dddb4248a5f0f5971e8f13d
Okjoo Han <okjoo.han88@gmail.com>
2023-06-02 22:59:17 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -3,7 +3,7 @@ function solution(n,a,b)
     if (n === 2) return 1;
     const times = n.toString(2).length - 1;
     const half = n / 2;
-    //console.log(n, a, b, times, half);
+    
     if ((a <= half && b > half) || (a > half && b <= half)) {
         return times;
     } else if (a <= half && b <= half) {
@@ -12,4 +12,14 @@ function solution(n,a,b)
         return solution(half, a - half, b - half);
     }
     return 0;
+    
+    /* 참고할 만한 코드 - Math.ceil() 활용
+    let answer = 0;
+    while (a !== b) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        answer++;
+    }
+    return answer;
+    */
 }
