652ca6373592eab64421c21e909b03d94ca7aa04
Okjoo Han <okjoo.han88@gmail.com>
2023-06-27 22:33:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -16,5 +16,8 @@ function solution(n)
     if (n === 1) return 1;
     const nArr = Array.from(n.toString(2));
     return nArr.reduce((a, b) => (+a) + (+b));
+    
+    2.
+    return n.toString(2).replace(/0/g, "").length;
     */   
 }
