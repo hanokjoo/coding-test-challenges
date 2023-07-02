ced8ae60db99db2eacbf573e7ea3abd48dea50dc
Okjoo Han <okjoo.han88@gmail.com>
2023-07-02 20:33:31 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -19,3 +19,16 @@ function getGCD(a, b) {
     }
     return a;
 }
+
+/* 참고할 만한 코드 - reduce() 활용
+function solution(arr) {
+    return arr.reduce((acc, cur) => {
+        const recursive = (min, max) => {
+          return (min % max) === 0 ? max : recursive(max, min % max);
+        }
+
+        let max = 0;
+        return acc*cur / recursive(acc,cur);
+    });
+}
+*/
