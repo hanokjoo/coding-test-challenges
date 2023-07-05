2f9d3f93a59951993cbbcd14985d416f778d1e34
Okjoo Han <okjoo.han88@gmail.com>
2023-07-05 21:40:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -75,4 +75,21 @@ function solution(s) {
 
     return answer;
 }
+
+2. 내 코드와 방법은 비슷한데 더 간결함
+function solution(s) {
+  let answer = 0;
+  for (let i = 0; i < s.length; i++) {
+    const arr = [];
+    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
+    for (let j = 0; j < temp.length; j++) {
+      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
+      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
+      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
+      else arr.push(temp[j]);
+    }
+    if (arr.length === 0) answer++;
+  }
+  return answer;
+}
 */
