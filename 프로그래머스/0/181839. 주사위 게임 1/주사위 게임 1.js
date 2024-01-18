3d3e092bfc3a43abc6618fa338faa24f9c0be116
Okjoo Han <okjoo.han88@gmail.com>
2024-01-18 21:35:33 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if (a % 2 === 0 && b % 2 === 0) {
+        return Math.abs(a - b);
+    } else if (a % 2 !== 0 && b % 2 !== 0) {
+        return Math.pow(a, 2) + Math.pow(b, 2);
+    } else {
+        return (a + b) * 2;
+    }
+    
+    return 0;
+}
