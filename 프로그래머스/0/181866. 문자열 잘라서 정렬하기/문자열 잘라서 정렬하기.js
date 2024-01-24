b0a4fd1f0a2546e6578c8a5e48bdb7087bdc8163
Okjoo Han <okjoo.han88@gmail.com>
2024-01-24 23:13:48 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 6.23 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -1,6 +1,8 @@
 function solution(myString) {
+    /** filter에서 걸러주니까 굳이 필요없음
     if (myString.startsWith("x")) myString = myString.substring(1);
-    if (myString.endsWith("x")) myString = myString.substring(0, myString.length - 1);    
+    if (myString.endsWith("x")) myString = myString.substring(0, myString.length - 1);
+    */
     
     return myString.split("x").filter((e) => e !== "").sort();
 }
