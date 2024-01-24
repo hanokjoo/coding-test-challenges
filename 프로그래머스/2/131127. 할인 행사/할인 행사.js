5afa06351a822428a3ebad2f6478902e806d0831
Okjoo Han <okjoo.han88@gmail.com>
2024-01-24 16:54:27 +0900

    [level 2] Title: 할인 행사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,20 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    
+    function countProd(arrDiscount) {
+        return arrDiscount.reduce((acc, cur) => {
+            if (cur in acc) acc[cur]++;
+            else acc[cur] = 1;
+            return acc;
+        }, {});
+    }
+    
+    for (let i = 0; i <= discount.length - 10; i++) {
+        let tenSale = countProd(discount.slice(i, i + 10));
+        if (want.map((e, i) => e = ((e in tenSale) && tenSale[e] === number[i])).every((e) => e)) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
