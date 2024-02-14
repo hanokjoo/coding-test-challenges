c891f61f82eea42fb9dbbb4a5c0a48c868cf1cc8
Okjoo Han <okjoo.han88@gmail.com>
2024-02-14 17:51:58 +0900

    [level 3] Title: 정수 삼각형, Time: 4.38 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,21 @@
+function solution(triangle) {    
+    let afterHap = triangle[triangle.length - 1];
+    for (let i = triangle.length - 1; i > 0; i--) {
+        afterHap = hap(triangle[i - 1], afterHap);
+    }
+    
+    //console.log(afterHap);
+    //console.log(triangle);
+    
+    return afterHap[0];
+}
+
+function hap(row1, row2) {
+    let result = [];
+    //console.log("hap:", row1, row2);
+    for (let i = 0; i < row1.length; i++) {
+        result.push(row1[i] + Math.max(row2[i], row2[i + 1]));
+    }
+    return result;
+}
+// start(16:02) ~ end (17:50)
