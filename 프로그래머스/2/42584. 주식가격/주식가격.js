0f561546c21e1251341fcb7cc3f3a2a872e3833f
Okjoo Han <okjoo.han88@gmail.com>
2024-08-08 21:24:40 +0900

    [level 2] Title: 주식가격, Time: 8.20 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -4,17 +4,14 @@ function solution(prices) {
     
     for (let i = 0; i < prices.length - 1; i++) {
         for (let j = i + 1; j < prices.length; j++) {
-            //console.log(i, prices[i], j, prices[j]);
             if (prices[i] > prices[j]) {
                 answer.push(j - i);
-                //console.log("> ", answer);
                 check = true;
                 break;
             }
         }
         if (!check) answer.push(prices.length - i - 1);
         check = false;
-        //console.log(">> ", answer);
     }
     
     answer.push(0);
