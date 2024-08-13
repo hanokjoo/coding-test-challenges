27220d212f0c6b8a2f025f53dd03bb28dd330b66
Okjoo Han <okjoo.han88@gmail.com>
2024-08-13 15:21:53 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,6 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
+    .toString()
+    .trim()
+    .split(" ");
+console.log(parseInt(input[0]) + parseInt(input[1]));
