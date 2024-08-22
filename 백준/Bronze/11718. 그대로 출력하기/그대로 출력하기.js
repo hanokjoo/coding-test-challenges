72a20c1a7c345c05e9a85453afd1a30643c47d07
Okjoo Han <okjoo.han88@gmail.com>
2024-08-22 13:07:56 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 100 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
+    .toString()
+    .trim()
+    .split("\n");
+
+for (let row of input) {
+    console.log(row);
+}
