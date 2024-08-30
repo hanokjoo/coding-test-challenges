f04d30a5485e1f95ab629f47f6d09c39684d0d6f
Okjoo Han <okjoo.han88@gmail.com>
2024-08-30 19:09:31 +0900

    [Bronze II] Title: 진법 변환, Time: 148 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -0,0 +1,7 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
+    .toString()
+    .trim()
+    .split(" ");
+
+console.log(parseInt(input[0], Number(input[1])));
