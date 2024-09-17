5efe8c23ca8a75722f457caabd0cab372eedf1e5
Okjoo Han <okjoo.han88@gmail.com>
2024-09-17 13:20:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.js
@@ -0,0 +1,9 @@
+let inputNum = Number(
+    require("fs")
+        .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
+        .toString()
+        .trim()
+);
+
+console.log(Math.pow(inputNum, 2));
+console.log(2);
