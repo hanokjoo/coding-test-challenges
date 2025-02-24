580814ed2e61690cc56e550018eaa2c37ec97206
Okjoo Han <okjoo.han88@gmail.com>
2025-02-24 16:34:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,26 @@
+let input = require("fs")
+    .readFileSync(process.platform === "linux" ? "dev/stdin" : "input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+
+let arrCount = [];
+
+if (input % 5 === 0 && input % 3 === 0) {
+    console.log(input / 5);
+    return;
+}
+
+let dividedFive = parseInt(input / 5);
+
+for (let i = 0; i <= dividedFive; i++) {   
+    const remainedWithoutFives = input - 5 * i;
+    if (remainedWithoutFives % 3 === 0) {
+        arrCount.push(i + remainedWithoutFives / 3);
+    }
+}
+
+if (arrCount.length > 0) {
+    console.log(Math.min(...arrCount));
+} else console.log(-1);
