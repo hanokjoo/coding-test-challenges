58bd3d659f112ded0334f553db95bb22f63bbbd9
Okjoo Han <okjoo.han88@gmail.com>
2025-02-26 23:28:42 +0900

    [Silver IV] Title: 보물, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.js
+++ 백준/Silver/1026. 보물/보물.js
@@ -1,28 +1,15 @@
-const [count, ...input] = require("fs")
-    .readFileSync("dev/stdin")
+const [count, ...input] = require("fs").readFileSync("dev/stdin")
     .toString()
     .trim()
     .split("\n");
 
 let arrA = input[0].replace("\r", "").split(" ").map(Number);
-const arrB = input[1].split(" ").map(Number);
-let arrC = new Array(arrB.length).fill(0);
-let [maxVal, maxIndex] = [-1, -1];
+let arrB = input[1].split(" ").map(Number);
 let answer = 0;
 arrA = arrA.sort((a, b) => b - a);
-
-while (arrA.length > 0) {
-    for (let i = 0; i < arrB.length; i++) {
-        if (arrC[i] === 0) {
-            if (maxVal <= arrB[i]) {
-                maxVal = arrB[i];
-                maxIndex = i;
-            }
-        }
-    }
-    answer += arrA.pop() * arrB[maxIndex];
-    arrC[maxIndex]++;
-    [maxVal, maxIndex] = [-1, -1];
+arrB = arrB.sort((a, b) => a - b);
+for (let i = 0; i < arrA.length; i++) {
+    answer += arrA[i] * arrB[i];
 }
 
 console.log(answer);
